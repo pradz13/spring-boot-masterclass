3a1f06217c75bf8ae9a93020f2a02afc5e3710fc
Pradipta <pradipta.nag2@gmail.com>
2023-02-07 21:07:51 +0530

    First commit for logging demo application

---
+++ logging-demo/src/main/java/com/springboot/masterclass/logging/DemoController.java
@@ -0,0 +1,21 @@
+package com.springboot.masterclass.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class DemoController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    @GetMapping("/hello")
+    public Map<String, String> hello(@RequestParam(defaultValue = "World") String name) {
+        logger.info("Name : {}", name);
+        return Map.of("greeting", " Hello " + name);
+    }
+}
