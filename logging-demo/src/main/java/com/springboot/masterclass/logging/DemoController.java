be62c0d07f3805dfcd1f20e3783f9206da39110d
Pradipta <pradipta.nag2@gmail.com>
2024-02-24 21:53:08 +0530

    Logging enhancements

--- logging-demo/src/main/java/com/springboot/masterclass/logging/DemoController.java
+++ logging-demo/src/main/java/com/springboot/masterclass/logging/DemoController.java
@@ -2,6 +2,7 @@ package com.springboot.masterclass.logging;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class DemoController {
 
     @GetMapping("/hello")
     public Map<String, String> hello(@RequestParam(defaultValue = "World") String name) {
+        //MDC.put("UserId", "1234");
         logger.debug("Name : {}", name);
         return Map.of("greeting", " Hello " + name);
     }
