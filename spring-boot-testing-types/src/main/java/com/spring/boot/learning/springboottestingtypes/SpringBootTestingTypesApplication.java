2e830148104bf075bf38e2513afeaa8b2fdac102
Pradipta <pradipta.nag2@gmail.com>
2024-02-24 22:07:35 +0530

    Adding files

---
+++ spring-boot-testing-types/src/main/java/com/spring/boot/learning/springboottestingtypes/SpringBootTestingTypesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.learning.springboottestingtypes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingTypesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestingTypesApplication.class, args);
+	}
+
+}
