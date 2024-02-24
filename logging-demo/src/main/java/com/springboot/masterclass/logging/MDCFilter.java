b3ede9ecffce4078db60705511e7f17b52fa669c
Pradipta <pradipta.nag2@gmail.com>
2024-02-24 21:53:46 +0530

    Logging enhancements - Adding the missing file

---
+++ logging-demo/src/main/java/com/springboot/masterclass/logging/MDCFilter.java
@@ -0,0 +1,25 @@
+package com.springboot.masterclass.logging;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class MDCFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            MDC.put("UserId", UUID.randomUUID().toString());
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+}
