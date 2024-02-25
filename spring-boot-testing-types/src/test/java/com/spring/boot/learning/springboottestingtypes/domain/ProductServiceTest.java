de453a0e54c0fd52cc4a61b4ad1f8150ba4b4e4d
Pradipta <pradipta.nag2@gmail.com>
2024-02-25 21:40:45 +0530

    Adding the test for ProductService

---
+++ spring-boot-testing-types/src/test/java/com/spring/boot/learning/springboottestingtypes/domain/ProductServiceTest.java
@@ -0,0 +1,39 @@
+package com.spring.boot.learning.springboottestingtypes.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+    @InjectMocks
+    private ProductService productService;
+
+    @Test
+    void shouldReturnOnlyActiveProducts() {
+        //Arrange
+        Product p1 = new Product(1L, "p-name1", "p-desc1", BigDecimal.TEN, false);
+        Product p2 = new Product(2L, "p-name2", "p-desc2", BigDecimal.TEN, true);
+        BDDMockito.given(productRepository.findAll()).willReturn(List.of(p1, p2));
+
+        //Act
+        List<Product> products = productService.getAllProducts();
+
+        //Assert
+        assertThat(products).hasSize(1);
+        assertThat(products.get(0).getId()).isEqualTo(1L);
+    }
+}
